8ede20af15e54c7bdc6873a968d9d4a49327649a
Humberto Filho <hr.soaresfilho@gmail.com>
2024-01-02 11:04:25 -0300

    Definição do layout base

---
+++ app/Views/template/footer.php
@@ -0,0 +1,3 @@
+<script src="<?= base_url('assets/bootstrap-5.3.2/js/bootstrap.bundle.min.js') ?>"></script>
+</body>
+</html>
---
+++ app/Views/template/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Auth Base System</title>
+    
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="<?= base_url('assets/bootstrap-5.3.2/css/bootstrap.min.css') ?>">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="<?= base_url('assets/fontawesome-free-6.5.1/css/all.min.css') ?>">   
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
+    
+</head>
+
+<body>
