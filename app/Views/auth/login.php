fde7de7bc6aab1866c6e022bc1ef92ee5e434bbb
Humberto Filho <hr.soaresfilho@gmail.com>
2024-01-02 11:05:06 -0300

    Criação das telas de login, registro e esqueceu a senha

---
+++ app/Views/auth/login.php
@@ -0,0 +1,48 @@
+<div id="main-wrapper" class="container mt-4">
+    <div class="row justify-content-center">
+        <div class="col-xl-10">
+            <div class="card border-0">
+                <div class="card-body p-0">
+                    <div class="row no-gutters">
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="mb-5">
+                                    <h3 class="h4 font-weight-bold"><a href="/" class="link-offset-2 link-underline link-underline-opacity-0"><i class="fa-solid fa-user-lock"></i> Auth Base System</a></h3>
+                                </div>
+
+                                <h6 class="h5 mb-0">Que bom que você voltou!</h6>
+                                <p class="text-muted mt-2 mb-5">Entre com a sua conta para acessar nossa plataforma</p>
+
+                                <form>
+                                    <div class="form-group mb-2">
+                                        <label for="email">E-mail</label>
+                                        <input type="email" class="form-control" id="email">
+                                    </div>
+                                    <div class="form-group mb-5">
+                                        <label for="password">Senha</label>
+                                        <input type="password" class="form-control" id="password">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Entrar</button>
+                                    <a href="forgot-password" class="forgot-link float-end text-primary">Esqueceu a senha?</a>
+                                </form>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6 d-none d-lg-inline-block">
+                            <div class="account-block rounded-right">
+                                <div class="overlay rounded-right"></div>
+                                <div class="account-testimonial">
+                                    <h4 class="text-white mb-4">Momento Autêntico</h4>
+                                    <p class="lead text-white">"Sorrisos são chaves que desbloqueiam corações."</p>
+                                    <p>- Administrador</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <p class="text-muted text-center mt-3 mb-0">Ainda não possui conta? <a href="register" class="text-primary ml-1">Registrar</a></p>
+        </div>
+    </div>
+</div>
